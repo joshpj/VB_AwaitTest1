5c96c52f6e1e00bb4c79ab102f88548ca753b7c3
Josh Jackson <joshpj12@gmail.com>
2018-08-06 23:09:21 -0400

    First commit

---
+++ AwaitTest1/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(109, 137)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(124, 34)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Blocking Call"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(109, 35)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(402, 62)
+        Me.TextBox1.TabIndex = 2
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(109, 199)
+        Me.ProgressBar1.MarqueeAnimationSpeed = 50
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(402, 21)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(387, 137)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(124, 34)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Await Call"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(109, 307)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(402, 228)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(106, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Output"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(106, 291)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(25, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Log"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(239, 137)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(142, 34)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Speed Up"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(640, 564)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ AwaitTest1/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+
+    Private Function ThingOne() As String
+        Return "Here comes..."
+    End Function
+
+    Private Function ThingTwo() As String
+        System.Threading.Thread.Sleep(5000)
+        Return vbCrLf & "Slowpoke" & vbCrLf
+    End Function
+
+    Private Function ThingThree() As String
+        Return "He's here!" & vbCrLf
+    End Function
+
+    Private Sub ClearBox()
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.AppendText("=== Blocking Test ===" & vbCrLf)
+        ClearBox()
+        TextBox2.AppendText("Start T1" & vbCrLf)
+        TextBox1.AppendText(ThingOne())
+        TextBox2.AppendText("Start T2" & vbCrLf)
+        TextBox1.AppendText(ThingTwo())
+        TextBox2.AppendText("Start T3" & vbCrLf)
+        TextBox1.AppendText(ThingThree())
+        TextBox2.AppendText("--- Test Complete ---" & vbCrLf)
+    End Sub
+
+    Private Async Function doThingTwo() As Task(Of String)
+        Return Await Task.Run(Function()
+                                  Return ThingTwo()
+                              End Function)
+    End Function
+
+    Private Async Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox2.AppendText("=== Await Test ===" & vbCrLf)
+        ClearBox()
+        TextBox2.AppendText("Start T1" & vbCrLf)
+        TextBox1.AppendText(ThingOne())
+        TextBox2.AppendText("Start T2" & vbCrLf)
+        TextBox1.AppendText(Await doThingTwo())
+        TextBox2.AppendText("Start T3" & vbCrLf)
+        TextBox1.AppendText(ThingThree())
+        TextBox2.AppendText("--- Test Complete ---" & vbCrLf)
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.PerformStep()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.AppendText(".")
+        TextBox2.AppendText("Speed-Up Button Click" & vbCrLf)
+    End Sub
+End Class
